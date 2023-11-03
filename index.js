c07c593cba9fcd22531aaf9d779f6167aca2a90d
Harsh Dahiya <123488839+believeharsh@users.noreply.github.com>
2023-11-03 16:04:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("hii Harsh Dahiya")
 // undestanding about git pull and fetch commands
-// remote updation here 
+// remote updation here : about git fetch
+// remote updation here : about git pull
